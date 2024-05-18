20c6f7ab99b00600574e4e21c00a16b6d0120623
rae charles <r_charles87@yahoo.com>
2024-05-18 15:08:52 -0400

    added seed.sql file to db and added users data

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+\c bite_ish;
+
+INSERT INTO users (first_name, last_name, user_name, password, phone_num) VALUES
+('test', 'test', 'besttester', '123456789', '7183201245'),
+('Rae', 'Charles', 'rcharles', '1234567890', '6469540083');
