4fdc20bc634c3dae65d75c2948668b87831a833a
rae charles <r_charles87@yahoo.com>
2024-05-20 18:55:42 -0400

    added users files to contoller and queries and added get all and get single users funtions and updated app.js routes to hit users

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+const usersController = require("./controllers/usersController.js")
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/users", usersController)
 
 app.get('/', (req,res)=>{
     res.status(200).json({data: 'Service is running'})
