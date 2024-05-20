72a385c96ba8aae1135ff44a751d881a9d1d6765
rae charles <r_charles87@yahoo.com>
2024-05-20 17:36:13 -0400

    added carts table and date to schema and seed files

--- db/seed.sql
+++ db/seed.sql
@@ -3,3 +3,10 @@
 INSERT INTO users (first_name, last_name, user_name, password, phone_num) VALUES
 ('test', 'test', 'besttester', '123456789', '7183201245'),
 ('Rae', 'Charles', 'rcharles', '1234567890', '6469540083');
+
+INSERT INTO carts (user_id, is_active) VALUES
+(1, false),
+(1, true),
+(2, false),
+(2, false),
+(2, true);
